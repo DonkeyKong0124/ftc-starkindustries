e23c24b34e0a03aa78280364d06c5793dd7b4e10
LohithaswaJ <lohithaswaj@gmail.com>
2024-12-14 17:05:06 -0800

    Update MyBasicOmniTeleOpMode.java

--- ftc-starkindustries-main/MyBasicOmniTeleOpMode.java
+++ ftc-starkindustries-main/MyBasicOmniTeleOpMode.java
@@ -31,16 +31,16 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize hardware variables with names matching the robot configuration.
-        rightFrontWheel = hardwareMap.get(DcMotor.class, "right_front"); // Forward
-        rightRearWheel = hardwareMap.get(DcMotor.class, "right_rear"); // Forward
-        leftFrontWheel = hardwareMap.get(DcMotor.class, "left_front"); // Backward
-        leftRearWheel = hardwareMap.get(DcMotor.class, "left_rear"); // Backward
+        rightFrontWheel = hardwareMap.get(DcMotor.class, "frontRight"); // Forward
+        rightRearWheel = hardwareMap.get(DcMotor.class, "backRight"); // Forward
+        leftFrontWheel = hardwareMap.get(DcMotor.class, "frontLeft"); // Backward
+        leftRearWheel = hardwareMap.get(DcMotor.class, "backLeft"); // Backward
 
-        towerArm = hardwareMap.get(DcMotor.class, "tower_arm");
-        foreArm = hardwareMap.get(DcMotor.class, "fore_arm"); // Initialize the Core Hex Motor
+        towerArm = hardwareMap.get(DcMotor.class, "towerArm");
+        foreArm = hardwareMap.get(DcMotor.class, "foreArm"); // Initialize the Core Hex Motor
 
-        intake = hardwareMap.get(Servo.class, "intake_servo");
-        claw = hardwareMap.get(Servo.class, "claw_servo");
+        intake = hardwareMap.get(Servo.class, "intake");
+        claw = hardwareMap.get(Servo.class, "claw");
 
         // Set motor directions.
         rightFrontWheel.setDirection(DcMotor.Direction.FORWARD);
@@ -66,54 +66,51 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
             double lateralPower = gamepad1.left_stick_x;
             // YAW - Clockwise and Anti-clockwise
             double yawPower = gamepad1.right_stick_x;
-
-            // Power for MOVING
-            double movePower = axialPower + lateralPower + yawPower;
-
+            
             // Set motor power for drivetrain.
-            rightFrontWheel.setPower(movePower);
-            rightRearWheel.setPower(movePower);
-            leftFrontWheel.setPower(movePower);
-            leftRearWheel.setPower(movePower);
-
-            // Tower arm to move Forward/Backward, on Y-axis of the Right joystick.
-            double towerArmPower = -gamepad1.right_stick_y;
-            towerArm.setPower(towerArmPower);
-
-            // Fore arm to move Forward/Backward on Right/Left Triggers.
-            if(gamepad1.right_trigger>0.0){
-                foreArm.setPower(gamepad1.right_trigger);
-            }
-            if(gamepad1.left_trigger>0.0){
-                foreArm.setPower(-gamepad1.left_trigger);
-            }
-
-            // PRESETS for Arms
-            if (gamepad1.dpad_up) {
-                towerArm.setPower(0.5);
-                foreArm.setPower(0.5);
-            } else if (gamepad1.dpad_down) {
-                towerArm.setPower(-0.5);
-                foreArm.setPower(-0.5);
-            }
-
-            // Intake using Right/Left Bumpers.
-            while (gamepad1.right_bumper) {
-                intake.setPosition(0.1); // Rotate servo forward
-            }
-            while (gamepad1.left_bumper) {
-                intake.setPosition(-0.1); // Rotate servo backward
-            }
-
-            // Claw Open/Close with X button
-            claw.setPosition(1.0); //  Start with an Open claw
-            if (gamepad1.x) { //while press and hold
-                claw.setPosition(0.2); // Close claw
-            } else { // when released
-                claw.setPosition(0.8); // Open claw
-            }
-
-            lastBump = gamepad1.x;
+            rightFrontWheel.setPower(axialPower - lateralPower - yawPower);
+            rightRearWheel.setPower(axialPower + lateralPower - yawPower);
+            leftFrontWheel.setPower(axialPower + lateralPower + yawPower);
+            leftRearWheel.setPower(axialPower - lateralPower + yawPower);
+//
+//            // Tower arm to move Forward/Backward, on Y-axis of the Right joystick.
+//            double towerArmPower = -gamepad1.right_stick_y;
+//            towerArm.setPower(towerArmPower);
+//
+//            // Fore arm to move Forward/Backward on Right/Left Triggers.
+//            if(gamepad1.right_trigger>0.0){
+//                foreArm.setPower(gamepad1.right_trigger);
+//            }
+//            if(gamepad1.left_trigger>0.0){
+//                foreArm.setPower(-gamepad1.left_trigger);
+//            }
+//
+//            // PRESETS for Arms
+//            if (gamepad1.dpad_up) {
+//                towerArm.setPower(0.5);
+//                foreArm.setPower(0.5);
+//            } else if (gamepad1.dpad_down) {
+//                towerArm.setPower(-0.5);
+//                foreArm.setPower(-0.5);
+//            }
+//
+//            // Intake using Right/Left Bumpers.
+//            while (gamepad1.right_bumper) {
+//                intake.setPosition(0.1); // Rotate servo forward
+//            }
+//            while (gamepad1.left_bumper) {
+//                intake.setPosition(-0.1); // Rotate servo backward
+//            }
+//
+//            // Claw Open/Close with X button
+//            claw.setPosition(1.0); //  Start with an Open claw
+//            if (gamepad1.x) { //while press and hold
+//                claw.setPosition(0.2); // Close claw
+//            } else { // when released
+//                claw.setPosition(0.8); // Open claw
+//            }
+//
+//            lastBump = gamepad1.x;
 
             // Display telemetry data.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
