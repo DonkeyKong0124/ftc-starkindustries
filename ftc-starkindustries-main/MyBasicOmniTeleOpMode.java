4fd3aa5235443760c072de9ed7f5a09be1904dfb
LohithaswaJ <lohithaswaj@gmail.com>
2024-12-25 08:32:45 -0800

    Update MyBasicOmniTeleOpMode.java

--- ftc-starkindustries-main/MyBasicOmniTeleOpMode.java
+++ ftc-starkindustries-main/MyBasicOmniTeleOpMode.java
@@ -3,12 +3,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Enhanced: 4-Motor Omni Drive with Dual Forearms and Claw", group="Linear OpMode")
+@TeleOp(name="uncle code", group="Linear OpMode")
 public class MyBasicOmniTeleOpMode extends LinearOpMode {
 
     // Declare OpMode members for motors and servos.
@@ -19,9 +20,9 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
     private DcMotor leftRearWheel = null; // Backward
 
     private DcMotor towerArm = null;
-    private DcMotor foreArm = null; // Declare REV Core Hex Motor here
+    private DcMotor foreArm1 = null; // Declare REV Core Hex Motor here
+    private DcMotor foreArm2 = null; // Declare REV Core Hex Motor here
 
-    private Servo intake = null;
     private Servo claw = null;
 
     private boolean lastBump = false;
@@ -37,9 +38,9 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
         leftRearWheel = hardwareMap.get(DcMotor.class, "backLeft"); // Backward
 
         towerArm = hardwareMap.get(DcMotor.class, "towerArm");
-        foreArm = hardwareMap.get(DcMotor.class, "foreArm"); // Initialize the Core Hex Motor
+        foreArm1 = hardwareMap.get(DcMotor.class, "foreArm1"); // Initialize the Core Hex Motor
+        foreArm2 = hardwareMap.get(DcMotor.class, "foreArm2"); // Initialize the Core Hex Motor
 
-        intake = hardwareMap.get(Servo.class, "intake");
         claw = hardwareMap.get(Servo.class, "claw");
 
         // Set motor directions.
@@ -48,18 +49,20 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
         leftFrontWheel.setDirection(DcMotor.Direction.REVERSE);
         leftRearWheel.setDirection(DcMotor.Direction.REVERSE);
 
-        towerArm.setDirection(DcMotor.Direction.FORWARD);
-        foreArm.setDirection(DcMotor.Direction.FORWARD); // Set Core Hex Motor direction
-
         // Display status and update telemetry.
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
         runtime.reset();
+        
+        claw.setPosition(1.0); //  Start with an Open claw
 
         // Main loop
         while (opModeIsActive()) {
+            
+            // ------------ WHEELS -------------- //
+            
             // AXIAL - Forward and Backward
             double axialPower = -gamepad1.left_stick_y;
             // LATERAL - Left and Right
@@ -69,57 +72,80 @@ public class MyBasicOmniTeleOpMode extends LinearOpMode {
             
             // Set motor power for drivetrain.
             rightFrontWheel.setPower(axialPower - lateralPower - yawPower);
-            rightRearWheel.setPower(axialPower + lateralPower - yawPower);
+            rightRearWheel.setPower(-axialPower + lateralPower - yawPower);
+            
             leftFrontWheel.setPower(axialPower + lateralPower + yawPower);
-            leftRearWheel.setPower(axialPower - lateralPower + yawPower);
-//
-//            // Tower arm to move Forward/Backward, on Y-axis of the Right joystick.
-//            double towerArmPower = -gamepad1.right_stick_y;
-//            towerArm.setPower(towerArmPower);
-//
-//            // Fore arm to move Forward/Backward on Right/Left Triggers.
-//            if(gamepad1.right_trigger>0.0){
-//                foreArm.setPower(gamepad1.right_trigger);
-//            }
-//            if(gamepad1.left_trigger>0.0){
-//                foreArm.setPower(-gamepad1.left_trigger);
-//            }
-//
-//            // PRESETS for Arms
-//            if (gamepad1.dpad_up) {
-//                towerArm.setPower(0.5);
-//                foreArm.setPower(0.5);
-//            } else if (gamepad1.dpad_down) {
-//                towerArm.setPower(-0.5);
-//                foreArm.setPower(-0.5);
-//            }
-//
-//            // Intake using Right/Left Bumpers.
-//            while (gamepad1.right_bumper) {
-//                intake.setPosition(0.1); // Rotate servo forward
-//            }
-//            while (gamepad1.left_bumper) {
-//                intake.setPosition(-0.1); // Rotate servo backward
-//            }
-//
-//            // Claw Open/Close with X button
-//            claw.setPosition(1.0); //  Start with an Open claw
-//            if (gamepad1.x) { //while press and hold
-//                claw.setPosition(0.2); // Close claw
-//            } else { // when released
-//                claw.setPosition(0.8); // Open claw
-//            }
-//
-//            lastBump = gamepad1.x;
+            leftRearWheel.setPower(-axialPower - lateralPower + yawPower);
+            
+
+            // ------------ TOWER ARM -------------- //
+            
+            // Tower arm to Extend/Retract, on Y-axis of the Right joystick.
+            double towerArmPower = -gamepad1.right_stick_y;
+            towerArm.setPower(towerArmPower);
+
+
+            // ------------ FORE ARM -------------- //
+            
+            // Fore arm to Extend on Left Trigger.
+            if(gamepad1.left_trigger>0){
+                foreArm1.setDirection(DcMotorSimple.Direction.FORWARD);
+                foreArm2.setDirection(DcMotorSimple.Direction.FORWARD);
+                foreArm1.setPower(gamepad1.left_trigger);     
+                foreArm2.setPower(gamepad1.left_trigger);     
+            }
+            // Fore arm to Retract on Right Trigger.
+            if(gamepad1.right_trigger>0){
+                foreArm1.setDirection(DcMotorSimple.Direction.REVERSE);
+                foreArm2.setDirection(DcMotorSimple.Direction.REVERSE);
+                foreArm1.setPower(gamepad1.right_trigger);         
+                foreArm2.setPower(gamepad1.right_trigger);   
+            }
+            
+            
+            // ------------ PRESETS -------------- //
+
+            // PRESETS for Arms
+            // if (gamepad1.dpad_up) {
+            //     foreArm1.setDirection(DcMotorSimple.Direction.REVERSE);
+            //     foreArm2.setDirection(DcMotorSimple.Direction.REVERSE);
+            //     foreArm1.setPower(1.0);
+            //     foreArm2.setPower(1.0);
+            //     towerArm.setPower(1.0); // <----   This is not working for the UltraPlanetary HD 
+            //     // foreArm1.setDirection(DcMotorSimple.Direction.FORWARD);
+            //     // foreArm2.setDirection(DcMotorSimple.Direction.FORWARD);
+            //     // foreArm1.setPower(1.0);
+            //     // foreArm2.setPower(1.0);
+            // } else if (gamepad1.dpad_down) {
+            //     foreArm1.setDirection(DcMotorSimple.Direction.REVERSE);
+            //     foreArm2.setDirection(DcMotorSimple.Direction.REVERSE);
+            //     foreArm1.setPower(1.0);
+            //     foreArm2.setPower(1.0);
+            //     towerArm.setPower(-1.0);// <----   This is not working for the UltraPlanetary HD 
+            // }
+            
+            // ------------ CLAW -------------- //
+
+            // Claw Open/Close 
+            if (gamepad1.y) { 
+                claw.setPosition(1.0); // Open claw
+            } 
+            if (gamepad1.a) { 
+                claw.setPosition(0.0); // Close claw
+            }
 
             // Display telemetry data.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motor Power", "LF (%.2f), RF (%.2f), LB (%.2f), RB (%.2f)",
                     leftFrontWheel.getPower(), rightFrontWheel.getPower(), leftRearWheel.getPower(), rightRearWheel.getPower());
             telemetry.addData("Towerarm Power", "%.2f", towerArm.getPower());
-            telemetry.addData("Fore Arm Power", "%.2f", foreArm.getPower()); // Telemetry for Core Hex Motor
-            telemetry.addData("Intake Servo Position", "%.2f", intake.getPosition());
+            telemetry.addData("Towerarm Direction", "%s", towerArm.getDirection());
+            telemetry.addData("Fore Arm1 Power", "%.2f", foreArm1.getPower()); 
+            telemetry.addData("Fore Arm1 Power", "%s", foreArm1.getDirection()); 
+            telemetry.addData("Fore Arm2 Power", "%.2f", foreArm2.getPower()); 
+            telemetry.addData("Fore Arm2 Power", "%s", foreArm2.getDirection()); 
             telemetry.addData("Claw Servo Position", "%.2f", claw.getPosition());
+            telemetry.addData("Claw Servo Direction", "%s", claw.getDirection()); 
             telemetry.update();
         }
     }
