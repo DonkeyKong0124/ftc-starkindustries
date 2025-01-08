7d076260fbc8997da3cfeada620920201d3308d0
LohithaswaJ <lohithaswaj@gmail.com>
2025-01-07 17:47:29 -0800

    Update MyStarterBot2025Autonomous.java

--- ftc-starkindustries-main/MyStarterBot2025Autonomous.java
+++ ftc-starkindustries-main/MyStarterBot2025Autonomous.java
@@ -38,34 +38,24 @@ import com.qualcomm.robotcore.util.Range;
 @Autonomous(name = "StarterBot_V1_Autonomous", group = "Robot")
 public class MyStarterBot2025Autonomous extends LinearOpMode {
 
+    static final double COUNTS_PER_MOTOR_REV = 1024;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
+    static final double WHEEL_DIAMETER_INCHES = 2.95;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+    static final double ARM_SPEED = 0.6;
     // Declare OpMode members for motors and servos.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor rightFrontWheel = null; // Forward
     private DcMotor rightRearWheel = null; // Forward
     private DcMotor leftFrontWheel = null; // Backward
     private DcMotor leftRearWheel = null; // Backward
-
     private DcMotor towerArm = null;
     private DcMotor foreArm1 = null; // Declare REV Core Hex Motor here
     private DcMotor foreArm2 = null; // Declare REV Core Hex Motor here
-
     private Servo claw = null;
-
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_MOTOR_REV    = 1024 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 2.95 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-    static final double     ARM_SPEED               = 0.3;
-
 
     @Override
     public void runOpMode() {
@@ -96,6 +86,7 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
         foreArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         foreArm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+
         rightFrontWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightRearWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftFrontWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -104,31 +95,62 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
         foreArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         foreArm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        //claw.setPosition(0.2); //  Start with an Closed claw
 
         // Display status and update telemetry.
         telemetry.addData("Status", "Initialized");
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Starting at",  "%7d :%7d :%7d :%7d",
+        telemetry.addData("Starting at", "%7d :%7d :%7d :%7d",
                 leftFrontWheel.getCurrentPosition(),
                 leftRearWheel.getCurrentPosition(),
                 rightFrontWheel.getCurrentPosition(),
                 rightRearWheel.getCurrentPosition());
+
         telemetry.update();
 
         waitForStart();
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED, 8,  8, 5.0);  // Move forward
-        encoderForeArms(ARM_SPEED, 5, 5.0);  // Extend ForeArm forward
-        encoderDrive(DRIVE_SPEED, -7.5, -7.5, 4.0);  // Move back
-        encoderDrive(TURN_SPEED,  -10.5, 10.5, 4.0);  // Turn left
-        encoderDrive(DRIVE_SPEED, 50,  50, 5.0);  // Move forward
+
+        encoderDrive(TURN_SPEED, -10.5, 10.5, 4.0);  // Turn left
+
+        // FROM HEREEEE
+        // encoderDrive(DRIVE_SPEED, 26,  26, 5.0);  // Move forward
+        // encoderDrive(TURN_SPEED,  10.5, -10.5, 4.0);  // Turn right
+        // claw.setPosition(0.0);                 //Open Claw
+        // encoderForeArms(ARM_SPEED, 2, 5.0);  // Extend ForeArm forward
+        // encoderDrive(DRIVE_SPEED, 6,  6, 5.0);  // Move forward
+        // claw.setPosition(1.0);                 //Close Claw
+        // encoderForeArms(ARM_SPEED, -2, 5.0);  // Retract ForeArm back
+        // encoderDrive(DRIVE_SPEED, -4, -4, 4.0);  // Move back
+        // encoderDrive(TURN_SPEED,  -15.5, 15.5, 4.0);  // Turn left
+        // encoderTowerArm(ARM_SPEED,35,5); // Towerarm lift
+        // encoderDrive(DRIVE_SPEED, 3,  3, 5.0);  // Move forward
+        // encoderForeArms(ARM_SPEED, 5, 5.0); // Extend ForeArm forward
+        // claw.setPosition(0.0);                 //Open Claw
+        // encoderForeArms(ARM_SPEED, -5, 5.0);  // Retract ForeArm back
+        // encoderTowerArm(ARM_SPEED,-35, 5); // Towerarm down
+        // encoderDrive(DRIVE_SPEED, -1,  -1, 5.0);  // Move backward
+        // encoderDrive(TURN_SPEED,  15.5, -15.5, 4.0);  // Turn right
+
+
+        // encoderDrive(TURN_SPEED,  2.3, -2, 4.0);  // Turn right
+        // encoderDrive(DRIVE_SPEED, -50,  -50, 5.0);  // Move back
+        // encoderDrive(TURN_SPEED,  10.5, -10.5, 4.0);  // Turn right
+        // encoderForeArms(ARM_SPEED, 2, 5.0); //Extend ForeArm forward
+        // encoderDrive(DRIVE_SPEED, 7,  7, 5.0); // Move foreword
+        // claw.setPosition(1.0);                 //Close Claw
+        // encoderDrive(DRIVE_SPEED, -5.2, -5.2, 4.0);  // Move back
+        // encoderDrive(TURN_SPEED,  -10.5, 10.5, 4.0);  // Turn left
+
+        // encoderYaw(DRIVE_SPEED, 5, -5, 5.0);  // Right strafe
+        // encoderYaw(DRIVE_SPEED, -5, 5, 5.0);  // Left strafe
+
+        //TO HEREEREEEEEE
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(60000);  // pause to display final telemetry message.
+        sleep(60000);  //1 min  pause to display final telemetry message.
 
     }
 
@@ -141,7 +163,7 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newTarget = towerArm.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+            newTarget = towerArm.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH);
 
             towerArm.setTargetPosition(newTarget);
 
@@ -152,24 +174,12 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
             runtime.reset();
             towerArm.setPower(Math.abs(speed));
 
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeouts) &&
-                    towerArm.isBusy()) {
-
-                // Display it for the driver.
-                telemetry.addData("TowerArm Running to",  " %7d", newTarget);
-                telemetry.addData("TowerArm Currently at",  "%7d",
-                        towerArm.getCurrentPosition());
-                telemetry.update();
-            }
-
             // Stop all motion;
             towerArm.setPower(0);
 
             // Turn off RUN_TO_POSITION
             towerArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            sleep(100);   // optional pause after each move.
         }
     }
 
@@ -182,11 +192,10 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newTarget = foreArm1.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+            newTarget = foreArm1.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH);
 
             foreArm1.setTargetPosition(newTarget);
             foreArm2.setTargetPosition(newTarget);
-
 
             // Turn On RUN_TO_POSITION
             foreArm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -197,17 +206,6 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
             foreArm1.setPower(Math.abs(speed));
             foreArm2.setPower(Math.abs(speed));
 
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeouts) &&
-                    (foreArm1.isBusy() || foreArm2.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("ForeArms Running to",  " %7d", newTarget);
-                telemetry.addData("ForeArms Currently at",  "%7d :%7d",
-                        foreArm1.getCurrentPosition(), foreArm2.getCurrentPosition());
-                telemetry.update();
-            }
-
             // Stop all motion;
             foreArm1.setPower(0);
             foreArm2.setPower(0);
@@ -216,7 +214,6 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
             foreArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             foreArm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            sleep(100);   // optional pause after each move.
         }
     }
 
@@ -231,8 +228,8 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeftTarget = leftFrontWheel.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = rightFrontWheel.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeftTarget = leftFrontWheel.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightFrontWheel.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
 
             leftFrontWheel.setTargetPosition(newLeftTarget);
             leftRearWheel.setTargetPosition(-newLeftTarget);
@@ -252,26 +249,6 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
             rightFrontWheel.setPower(Math.abs(speed));
             rightRearWheel.setPower(Math.abs(speed));
 
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeouts) &&
-                    (leftFrontWheel.isBusy() || rightFrontWheel.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Wheels Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
-                telemetry.addData("Wheels Currently at",  "%7d :%7d :%7d :%7d",
-                        leftFrontWheel.getCurrentPosition(),
-                        leftRearWheel.getCurrentPosition(),
-                        rightFrontWheel.getCurrentPosition(),
-                        rightRearWheel.getCurrentPosition());
-                telemetry.update();
-            }
-
             // Stop all motion;
             leftFrontWheel.setPower(0);
             leftRearWheel.setPower(0);
@@ -284,7 +261,6 @@ public class MyStarterBot2025Autonomous extends LinearOpMode {
             rightFrontWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightRearWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            sleep(100);   // optional pause after each move.
         }
     }
 }
